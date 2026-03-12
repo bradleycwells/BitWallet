12de8d1b470b91f69115c4aab72cf0e07bfb9df6
Bradley Wells <bradley@sensornetworks.co.za>
2026-03-12 10:23:09 +0200

    moved logoview to core/views for state wide components

--- BitWallet/Core/Views/LogoView.swift
+++ BitWallet/Core/Views/LogoView.swift
@@ -48,26 +48,3 @@ struct LogoView: View {
         isAnimatingText = false
     }
 }
-
-
-#Preview {
-    StatefulPreviewWrapper(false) { binding in
-        LogoView(animate: binding)
-    }
-}
-
-// Helper to provide a binding in previews
-struct StatefulPreviewWrapper<Value, Content: View>: View {
-    @State private var value: Value
-    private let content: (Binding<Value>) -> Content
-
-    init(_ initialValue: Value, content: @escaping (Binding<Value>) -> Content) {
-        _value = State(wrappedValue: initialValue)
-        self.content = content
-    }
-
-    var body: some View {
-        content($value)
-    }
-}
-
